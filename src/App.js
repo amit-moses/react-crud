b14385d92115589a15f43d3547401aaa8f616315
amit-moses <amitm747@gmail.com>
2023-11-24 09:55:46 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ function App() {
         <Route path="/edit" element={<Edit api_url={api_url} />} />
         <Route path="/mycart" element={<Cart api_url={api_url} refresh_cart={refresh_func} cartList={cartList} total_to_pay={totalPay} cart_data={cartData} />} />
         <Route path="/*" element={<Home api_url={api_url} refresh_cart={refresh_func} cartList={cartList} />} />
-        {/* <Route path="/*" element={<div>no page</div>} /> */}
       </Routes>
     </BrowserRouter>
   );
